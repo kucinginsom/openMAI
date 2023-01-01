5d812d018c25de8f2a7237af1dd454935dc60366
kucinginsom <kucinginsom@gmail.com>
2023-01-01 20:58:16 +0700

    add be online

--- client/script.js
+++ client/script.js
@@ -72,7 +72,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv);
 
     //Fetch data from bot server openai
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://courageous-pig-wear.cyclic.app/be', {
         method: 'POST',
         headers: {
             'Content-Type' : 'application/json',
